1808fdf31a4c8a60f9528c770ffd1d5fc161ccb5
vadflor <vadflor@gmail.com>
2022-11-02 16:24:31 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from "next/head";
+
+import { MongoClient } from "mongodb";
+import MeetupList from "../components/meetups/MeetupList";
+
+const index = (props) => {
+    return (
+        <>
+            <Head>
+                <title>React Meetups</title>
+                <meta name="description" content="Browse a huge list of highly active React meetups!" />
+            </Head>
+            <MeetupList meetups={props.meetups} />;
+        </>
+    );
+};
+
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// };
+
+export async function getStaticProps() {
+    const client = await MongoClient.connect(
+        "mongodb+srv://next:nextjs@cluster0.speep3b.mongodb.net/meetup?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const meetups = await meetupsCollection.find().toArray();
+    client.close();
+
+    return {
+        props: {
+            meetups: meetups.map((meetup) => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString(),
+            })),
+        },
+        revalidate: 1,
+    };
+}
+
+export default index;
